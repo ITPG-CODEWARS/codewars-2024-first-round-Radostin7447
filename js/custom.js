46c49004c9dc99880d8f9fd951e12edb3f670cfa
radostin74 <radostinhristov36@gmail.com>
2024-10-28 17:35:11 +0200

    5

--- js/custom.js
+++ js/custom.js
@@ -47,173 +47,7 @@ jQuery(function ($) {
       }
    }
 
-   if ($('#map').length > 0) {
 
-      var contactmap = {
-         lat: -37.816218,
-         lng: 144.964068
-      };
-
-      $('#map')
-         .gmap3({
-            zoom: 12,
-            center: contactmap,
-            scrollwheel: false,
-            mapTypeId: "shadeOfGrey",
-            mapTypeControlOptions: {
-               mapTypeIds: [google.maps.MapTypeId.ROADMAP, "shadeOfGrey"]
-            }
-         })
-
-         .styledmaptype(
-            "shadeOfGrey", [
-
-               {
-                  "featureType": "administrative",
-                  "elementType": "geometry.stroke",
-                  "stylers": [{
-                     "color": "#fefefe"
-                  }, {
-                     "lightness": 17
-                  }, {
-                     "weight": 1.2
-                  }]
-               },
-               {
-                  "featureType": "administrative",
-                  "elementType": "geometry.fill",
-                  "stylers": [{
-                     "color": "#fefefe"
-                  }, {
-                     "lightness": 20
-                  }]
-               },
-               {
-                  "featureType": "transit",
-                  "elementType": "geometry",
-                  "stylers": [{
-                     "color": "#f2f2f2"
-                  }, {
-                     "lightness": 19
-                  }]
-               },
-               {
-                  "featureType": "all",
-                  "elementType": "labels.icon",
-                  "stylers": [{
-                     "visibility": "off"
-                  }]
-               },
-               {
-                  "featureType": "all",
-                  "elementType": "labels.text.fill",
-                  "stylers": [{
-                     "saturation": 36
-                  }, {
-                     "color": "#333333"
-                  }, {
-                     "lightness": 40
-                  }]
-               },
-               {
-                  "featureType": "all",
-                  "elementType": "labels.text.stroke",
-                  "stylers": [{
-                     "visibility": "on"
-                  }, {
-                     "color": "#ffffff"
-                  }, {
-                     "lightness": 16
-                  }]
-               },
-               {
-                  "featureType": "poi",
-                  "elementType": "geometry",
-                  "stylers": [{
-                     "color": "#f5f5f5"
-                  }, {
-                     "lightness": 21
-                  }]
-               },
-               {
-                  "featureType": "road.local",
-                  "elementType": "geometry",
-                  "stylers": [{
-                     "color": "#ffffff"
-                  }, {
-                     "lightness": 16
-                  }]
-               },
-               {
-                  "featureType": "road.arterial",
-                  "elementType": "geometry",
-                  "stylers": [{
-                     "color": "#ffffff"
-                  }, {
-                     "lightness": 18
-                  }]
-               },
-               {
-                  "featureType": "road.highway",
-                  "elementType": "geometry.stroke",
-                  "stylers": [{
-                     "color": "#ffffff"
-                  }, {
-                     "lightness": 29
-                  }, {
-                     "weight": 0.2
-                  }]
-               },
-               {
-                  "featureType": "road.highway",
-                  "elementType": "geometry.fill",
-                  "stylers": [{
-                     "color": "#ffffff"
-                  }, {
-                     "lightness": 17
-                  }]
-               },
-               {
-                  "featureType": "landscape",
-                  "elementType": "geometry",
-                  "stylers": [{
-                     "color": "#f5f5f5"
-                  }, {
-                     "lightness": 20
-                  }]
-               },
-               {
-                  "featureType": "water",
-                  "elementType": "geometry",
-                  "stylers": [{
-                     "color": "#e9e9e9"
-                  }, {
-                     "lightness": 17
-                  }]
-               }
-            ], {
-               name: "HQ"
-            }
-         )
-
-         .marker({
-            position: contactmap,
-            icon: 'images/map-icon.png'
-         })
-
-         .infowindow({
-            position: contactmap,
-            content: "16122 Collins Street West. Victoria"
-         })
-
-         .then(function (infowindow) {
-            var map = this.get(0);
-            var marker = this.get(1);
-            marker.addListener('click', function () {
-               infowindow.open(map, marker);
-            });
-         });
-   }
 
 
    $('#main-slide').carousel({
@@ -374,14 +208,12 @@ jQuery(function ($) {
          var $name = $form.find('.form-name'),
             $phone = $form.find('.form-phone'),
             $email = $form.find('.form-email'),
-            $url = $form.find('.form-website'),
             $message = $form.find('.form-message');
 
          $.post(action, {
                name: $name.val(),
                phone: $phone.val(),
                email: $email.val(),
-               url: $url.val(),
                message: $message.val()
             },
             function (data) {
@@ -392,7 +224,6 @@ jQuery(function ($) {
                   $name.val('');
                   $phone.val('');
                   $email.val('');
-                  $url.val('');
                   $message.val('');
                }
             }
